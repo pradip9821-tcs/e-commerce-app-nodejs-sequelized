232df1e0e3312162bcce5f2e80a1556e61667a7d
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-03-03 10:48:24 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,6 +1,7 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('node-complete','root','12345678',{
+//Enter your database password to create database connection pool.
+const sequelize = new Sequelize('node-complete','root','<#ENTER_YOUR_DATABASE_PASSWORD>',{
     dialect : 'mysql',
     host : 'localhost'
 });
